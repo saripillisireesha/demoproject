a5a734403623f765b9a916b8091f8998f67f2a19
saripillisireesha <95239362+saripillisireesha@users.noreply.github.com>
2024-07-12 11:20:12 -0400

    del

--- src/main/java/com/employ/registration/constants/ApplicationConstants.java
+++
@@ -1,6 +0,0 @@
-package com.employ.registration.constants;
-
-public class ApplicationConstants {
-	public static final String Employ_Not_Found="Employ not found";
-
-}
--- src/main/java/com/employ/registration/handler/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.employ.registration.handler;
-
-public class ResourceNotFoundException extends RuntimeException{
-        public ResourceNotFoundException(String msg) {
-        	super(msg);
-        }
-}
